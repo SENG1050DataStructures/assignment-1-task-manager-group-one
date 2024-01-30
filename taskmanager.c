f23bf8520c9a97362abb18ebf3e1441c2f7fbd69
joeyuen23 <joeyuen23@gmail.com>
2024-01-29 21:41:04 -0500

    struct declared

--- taskmanager.c
+++ taskmanager.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#include KTitleLen 101
+#include KDescriptionLen 201
+
+struct Task {
+    int TaskId;
+    char Title[KTitleLen];
+    char Description[KDescriptionLen];
+    struct Task* NextTask;
+};
+
 int main(void)
 {
     
